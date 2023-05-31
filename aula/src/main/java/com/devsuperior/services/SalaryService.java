341d09a4d3fe28f1db58673f010c4592ffbbe497
Grasiela Texeira <grasy.mst@gmail.com>
2023-05-31 10:36:30 -0300

    Iniciando primeiro Projeto em Spring

---
+++ aula/src/main/java/com/devsuperior/services/SalaryService.java
@@ -0,0 +1,18 @@
+package com.devsuperior.services;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class SalaryService {
+
+    @Autowired
+    private com.devsuperior.aulaspring.services.TaxService taxService;
+
+    @Autowired
+    private PensionService pensionService;
+
+    public double netSalary(double grossSalary) {
+        return grossSalary - taxService.tax(grossSalary) - pensionService.discount(grossSalary);
+    }
+}
